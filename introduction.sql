9f7225c7f7ea87162be56508ae56949af8e98c58
Md Zahangir Alam(#Mr.) <latestworld9042@gmail.com>
2023-02-02 19:48:24 +0600

    Create introduction.sql

---
+++ introduction.sql
@@ -0,0 +1,5 @@
+-- Create Database
+create database mydatabase;
+
+-- Create Table
+create table aubians(Name varhcar(255), Id int(159), Email varhcar(255), Phone varchar(11), Department varchar(5), Section varchar(1), Batch int(3), Address, varchar(255));
