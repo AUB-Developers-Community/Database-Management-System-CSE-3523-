d97c0feb19a9943d7dd155f7088ff8307428d53b
Md Zahangir Alam(#Mr.) <latestworld9042@gmail.com>
2023-02-05 17:52:28 +0600

    init

---
+++ Lab/secondLab/queries.sql
@@ -0,0 +1,14 @@
+-- operators
+-- or
+-- and
+-- not
+
+select * from student where name="Jahangir" or name="Sakib";
+
+
+
+select * from student where name="Sakib" and id=13;
+
+
+
+select * from student where name="Suma" and not id=24;
