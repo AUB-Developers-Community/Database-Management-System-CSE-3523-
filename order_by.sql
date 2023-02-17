3fde5efbc6fa4928170411f575d97ec34f247610
Md Zahangir Alam(#Mr.) <latestworld9042@gmail.com>
2023-02-17 19:21:59 +0600

    Update order_by.sql

--- order_by.sql
+++ order_by.sql
@@ -2,6 +2,7 @@ select * from <table_name> order by <column_name>; -- default in ascending order
 
 select * from <table_name> order by <column_name> desc; -- order in descending order
 
-select * from <table_name> order by <column_name1>, <column_name2>, <column_name3>;
+select * from <table_name> order by <column_name1>, <column_name2>, <column_name3>; -- several columns
 
+select * from <table_name> order by <column_name1> asc, <column_name2> desc, <column_name3> asc; -- different column in different order
 
