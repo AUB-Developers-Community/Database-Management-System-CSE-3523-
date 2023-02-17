293d0e13c76fb70d6e39a47f64a640b1e167fe10
Md Zahangir Alam(#Mr.) <latestworld9042@gmail.com>
2023-02-17 19:53:08 +0600

    Update delete.sql

--- delete.sql
+++ delete.sql
@@ -1,1 +1,3 @@
-delete from <table_name> where <column_name>=<column_value>;
+delete from <table_name> where <column_name>=<column_value>; -- delete single specific value
+
+delete from <table_name>; -- delete whole records
