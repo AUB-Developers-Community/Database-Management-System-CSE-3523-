1f13dff087661ec419d2d2b7dedd64ddd1a57d7e
Md Zahangir Alam(#Mr.) <latestworld9042@gmail.com>
2023-02-17 19:14:33 +0600

    Update order_by.sql

--- order_by.sql
+++ order_by.sql
@@ -1,1 +1,7 @@
 select * from <table_name> order by <column_name>; -- default in ascending order
+
+select * from <table_name> order by <column_name> desc; -- order in descending order
+
+select * from <table_name> order by <column_name1>, <column_name2>, <column_name3>;
+
+
