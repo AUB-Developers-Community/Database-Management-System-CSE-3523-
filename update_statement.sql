8aca7c320da87ba0b37752149f53ae0ae10c2a60
Md Zahangir Alam(#Mr.) <latestworld9042@gmail.com>
2023-02-17 19:48:55 +0600

    Create update_statement.sql

---
+++ update_statement.sql
@@ -0,0 +1,2 @@
+update <table_name> set <column_name>=<column_value> where <column_name>=<column_value>; -- update single column value
+update <table_name> set <column_name1>=<column_value1>, <column_name2>=<column_value2> where <column_name>=<column_value>; -- update several column value
